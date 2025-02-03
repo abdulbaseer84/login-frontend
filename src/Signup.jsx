19819d025c9d41f64635159b6450843604a73f60
Abdul Baseer <124512845+abdulbaseer84@users.noreply.github.com>
2025-02-03 17:02:58 +0530

    Update Signup.jsx

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -9,7 +9,7 @@ function Signup(){
     const navigate=useNavigate()
     const handleSubmit=(e)=>{
         e.preventDefault()
-        axios.post('http://localhost:3001/register',{name,email,password})
+        axios.post('https://login-backend-weld.vercel.app/register',{name,email,password})
         .then(result=>{console.log(result)
         navigate('/home')
         })
